6edc1d9e8ef1b69c87e061fc2a944e4ffeb65b0b
shruti942 <jagtapshruti2610@gmail.com>
2021-07-10 16:44:35 +0530

    student

--- student/urls.py
+++ student/urls.py
@@ -23,4 +23,12 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     # path('', views.login, redirect('student:login')),
     # TODO: add redirect for '' to login
+path('student-exam', views.student_exam_view,name='student-exam'),
+path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
+path('start-exam/<int:pk>/', views.start_exam_view,name='start-exam'),
+path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
+path('view-result', views.view_result_view,name='view-result'),
+path('check-marks/<int:pk>', views.check_marks_view,name='check-marks'),
+path('student-marks', views.student_marks_view,name='student-marks'),
+
 ]
