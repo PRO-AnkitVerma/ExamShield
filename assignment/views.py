acb5cf9f6d98fb9f2e3318c71b57d9bc252a3978
Sagar Desai <desai.sagar2002@gmail.com>
2021-07-10 23:33:04 +0530

    > create Submit Assignment Template

--- assignment/views.py
+++ assignment/views.py
@@ -6,11 +6,8 @@ from mysite.decorators import allowed_users
 
 @allowed_users(allowed_groups=['faculty'])
 def create_assignment(request):
-    if request.method == 'GET':
-        return HttpResponse('Create Assignment Page')
-
-    if request.method == 'POST':
-        return HttpResponse('Validating Creation of Assignment')
-
-
     return render(request, 'assignment/create-assignment.html')
+
+@allowed_users(allowed_groups=['faculty'])
+def create_assignment(request):
+    return render(request, 'assignment/submit-assignment.html')
