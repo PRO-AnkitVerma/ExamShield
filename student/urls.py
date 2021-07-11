7291b642ecc5738809bab064cbd23eaf3332ed7b
Sagar Desai <desai.sagar2002@gmail.com>
2021-07-11 19:28:47 +0530

    > Create Student templates

--- student/urls.py
+++ student/urls.py
@@ -24,12 +24,12 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     # path('', views.login, redirect('student:login')),
     # TODO: add redirect for '' to login
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('student-exam', views.student_exam_view,name='student-exam'),
-    path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
-    path('start-exam/<int:pk>/', views.start_exam_view,name='start-exam'),
-    path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
+    path('dashboard/', views.student_dashboard_view, name='dashboard'),
     path('view-result', views.view_result_view,name='view-result'),
     path('check-marks/<int:pk>', views.check_marks_view,name='check-marks'),
     path('student-marks', views.student_marks_view,name='student-marks'),
+    path('start-exam/<int:pk>/', views.start_exam_view,name='start-exam'),
+    path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
+    path('student-exam', views.student_exam_view,name='student-exam'),
+    path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
 ]
