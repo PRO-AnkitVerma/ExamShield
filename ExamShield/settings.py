9af29d793df6801765751e1598ef6386408516d2
jil patel <jil1704.jp@gmail.com>
2021-07-17 20:28:05 +0530

    commit

--- ExamShield/settings.py
+++ ExamShield/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3v@hoyy!al@dycs5s^+!wcurkie^^m7uchys4=t@j=l9z35ap9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- ExamShield/urls.py
+++ ExamShield/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # technical team
@@ -27,6 +29,8 @@ urlpatterns = [
     path('question/', include('question.urls')),
     path('subject/', include('subject.urls')),
     path('assignment/', include('assignment.urls')),
+    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
